bc03e2d6e24432d5b61385f4c884a3d289f973bc
Steffen Schmidt <50168768+SockSteve@users.noreply.github.com>
2024-02-01 13:08:36 +0100

    fixed movement bug

--- GodotProject/Scripts/Characters/Player/States/Run.gd
+++ GodotProject/Scripts/Characters/Player/States/Run.gd
@@ -23,12 +23,14 @@ func physics_update(delta: float) -> void:
 	var y_velocity := player.velocity.y
 	player.velocity.y = 0.0
 	player.velocity = player.velocity.lerp(player._move_direction * player.move_speed, player.acceleration * delta)
+
 	if player._move_direction.length() == 0 and player.velocity.length() < player.stopping_speed:
 		player.velocity = Vector3.ZERO
 	player.velocity.y = y_velocity
 	player.velocity.y += player._gravity * delta
 	
 	var xz_velocity := Vector3(player.velocity.x, 0, player.velocity.z)
+	
 	player._character_skin.set_moving(true)
 	player._character_skin.set_moving_speed(inverse_lerp(0.0, player.move_speed, xz_velocity.length()))
 	
@@ -36,7 +38,7 @@ func physics_update(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump"):
 		state_machine.transition_to("Air", {do_jump = true})
-	elif is_equal_approx(player.velocity.x, 0.0) and is_equal_approx(player.velocity.y, 0.0):
+	elif is_equal_approx(player.velocity.x, 0.0) and is_equal_approx(player.velocity.z, 0.0):
 		state_machine.transition_to("Idle")
 	elif Input.is_action_pressed("gadget"):
 		state_machine.transition_to("Grapple")
