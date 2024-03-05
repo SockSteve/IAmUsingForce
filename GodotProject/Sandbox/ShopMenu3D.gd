9135e7b71bb9975ab0121d37c7bb4e5459436232
Steffen Schmidt <50168768+SockSteve@users.noreply.github.com>
2024-03-05 15:25:05 +0100

    placeholder_icons

--- GodotProject/Sandbox/ShopMenu3D.gd
+++ GodotProject/Sandbox/ShopMenu3D.gd
@@ -31,7 +31,6 @@ func cleanup():
 func _mouse_entered_area():
 	is_mouse_inside = true
 
-
 func _unhandled_input(event):
 	# Check if the event is a non-mouse/non-touch event
 	var is_mouse_event = false
@@ -46,19 +45,18 @@ func _unhandled_input(event):
 	elif not is_mouse_event:
 		node_viewport.push_input(event)
 
-
 # Handle mouse events inside Area. (Area.input_event had many issues with dragging)
 func handle_mouse(event):
 	# Get mesh size to detect edges and make conversions. This code only support PlaneMesh and QuadMesh.
 	quad_mesh_size = node_quad.mesh.size
-
+	
 	# Detect mouse being held to mantain event while outside of bounds. Avoid orphan clicks
 	if event is InputEventMouseButton or event is InputEventScreenTouch:
 		is_mouse_held = event.pressed
-
+		
 	# Find mouse position in Area
 	var mouse_pos3D = find_mouse(event.global_position)
-
+	
 	# Check if the mouse is outside of bounds, use last position to avoid errors
 	# NOTE: mouse_exited signal was unrealiable in this situation
 	is_mouse_inside = mouse_pos3D != null
@@ -71,12 +69,10 @@ func handle_mouse(event):
 		mouse_pos3D = last_mouse_pos3D
 		if mouse_pos3D == null:
 			mouse_pos3D = Vector3.ZERO
-
-	# TODO: adapt to bilboard mode or avoid completely
-
+	
 	# convert the relative event position from 3D to 2D
 	var mouse_pos2D = Vector2(mouse_pos3D.x, -mouse_pos3D.y)
-
+	
 	# Right now the event position's range is the following: (-quad_size/2) -> (quad_size/2)
 	# We need to convert it into the following range: 0 -> quad_size
 	mouse_pos2D.x += quad_mesh_size.x / 2
@@ -84,16 +80,16 @@ func handle_mouse(event):
 	# Then we need to convert it into the following range: 0 -> 1
 	mouse_pos2D.x = mouse_pos2D.x / quad_mesh_size.x
 	mouse_pos2D.y = mouse_pos2D.y / quad_mesh_size.y
-
+	
 	# Finally, we convert the position to the following range: 0 -> viewport.size
 	mouse_pos2D.x = mouse_pos2D.x * node_viewport.size.x
 	mouse_pos2D.y = mouse_pos2D.y * node_viewport.size.y
 	# We need to do these conversions so the event's position is in the viewport's coordinate system.
-
+	
 	# Set the event's position and global position.
 	event.position = mouse_pos2D
 	event.global_position = mouse_pos2D
-
+	
 	# If the event is a mouse motion event...
 	if event is InputEventMouseMotion:
 		# If there is not a stored previous position, then we'll assume there is no relative motion.
@@ -105,19 +101,19 @@ func handle_mouse(event):
 			event.relative = mouse_pos2D - last_mouse_pos2D
 	# Update last_mouse_pos2D with the position we just calculated.
 	last_mouse_pos2D = mouse_pos2D
-
+	
 	# Finally, send the processed input event to the viewport.
 	node_viewport.push_input(event)
 
 
 func find_mouse(global_position):
 	var camera = get_viewport().get_camera_3d()
-
+	
 	# From camera center to the mouse position in the Area
 	var from = camera.project_ray_origin(global_position)
 	var dist = find_further_distance_to(camera.transform.origin)
 	var to = from + camera.project_ray_normal(global_position) * dist
-
+	
 	# Manually raycasts the are to find the mouse position
 	var ray_query = PhysicsRayQueryParameters3D.create(from, to, node_area.collision_layer,[])
 	ray_query.collide_with_areas = true
@@ -145,7 +141,7 @@ func find_further_distance_to(origin):
 		temp_dist = origin.distance_to(edge)
 		if temp_dist > far_dist:
 			far_dist = temp_dist
-
+	
 	return far_dist
 
 
