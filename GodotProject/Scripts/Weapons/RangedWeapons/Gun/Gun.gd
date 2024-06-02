87447688131f1ca0ee698223256d2287727a27a4
Steffen Schmidt <50168768+SockSteve@users.noreply.github.com>
2024-06-02 22:25:16 +0200

    weapon class gucci

--- GodotProject/Scripts/Weapons/RangedWeapons/Gun/Gun.gd
+++ GodotProject/Scripts/Weapons/RangedWeapons/Gun/Gun.gd
@@ -4,23 +4,23 @@ extends Weapon
 @export var bullet_speed : float = 100.0
 @export var fire_rate : float = 0.25
 
-var can_shoot = true
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var bullet_spawn_marker: Marker3D = $Marker3D
 
-func _ready():
-	pass
+var can_shoot: bool = true
 
-func _process(delta):
+func _process(delta)->void:
 	if can_shoot and Input.is_action_pressed("ranged_attack"):
 		shoot()
 
-func shoot():
-	var bullet = bullet.instantiate()
+func shoot()->void:
+	var bullet: Node3D = bullet.instantiate()
+	
 	#add child to character out of rotational root detached from any player transform
 	BulletContainer.add_child(bullet)
-	#get_tree().root.add_child(bullet)
 
 	# Set the bullet's position to the gun's position
-	bullet.global_transform.origin = global_transform.origin
+	bullet.global_transform.origin = bullet_spawn_marker.global_transform.origin
 
 	# Apply velocity to the bullet
 	var direction = global_transform.basis.z.normalized()
@@ -28,5 +28,7 @@ func shoot():
 
 	# Implement fire rate
 	can_shoot = false
-	await get_tree().create_timer(fire_rate).timeout
+	fire_rate_timer.start(fire_rate)
+
+func _on_fire_rate_timer_timeout():
 	can_shoot = true
--- GodotProject/Scripts/Weapons/Weapon.gd
+++ GodotProject/Scripts/Weapons/Weapon.gd
@@ -1,26 +1,34 @@
 extends Node3D
 class_name Weapon
 
+@export_category("Weapon")
+
+@export_group("Basics")
 @export var _name : String = ""
 @export var upgrade_name: String = ""
 @export var _attributes : Dictionary = {}
-
 @export var icon : Texture2D
+@export var base_weapon_mesh: Mesh = null
+@export var upgraded_weapon_mesh: Mesh = null
+@export_group("Shop")
 @export var shop_price: int = 0
 @export var upgrade_shop_price: int = 0
 @export var bullet_price: int = 0
+@export_group("Combat")
 @export var damage: int = 1
+@export var knockback := 0.0
+@export var stagger := 0.0
 @export var current_ammunition: int = 1
 @export var max_ammunition: int = 1
 @export var current_lvl: int = 1
 @export var max_lvl: = 7
 @export var current_experience: int = 0
-
-@export var knockback := 0.0
-@export var stagger := 0.0
-
 @export var bullet : PackedScene = null
 
+@export_subgroup("Melee")
+@export var max_combo_steps: int = 1
+
+@export_group("Level Specifics")
 @export var max_experience_per_lvl : Dictionary ={}		#dictionary with all xp values
 @export var damage_per_lvl : Dictionary = {}			#dictionary with all dmg values
 
@@ -29,7 +37,8 @@ func init(id, base_name, attributes):
 	_attributes = attributes
 
 func _ready():
-	_name = self.get_name()
+	if name == "":
+		_name = self.get_name()
 
 func lvl_up():
 	current_lvl += 1
