cd2fe3d8904d7e5c7adf305ef1449b367b9f356d
Steffen Schmidt <50168768+SockSteve@users.noreply.github.com>
2024-07-26 11:06:35 +0200

    debug shotgun

--- GodotProject/Scripts/Weapons/RangedWeapons/ShrapnelCannon/ShrapnelCannon.gd
+++ GodotProject/Scripts/Weapons/RangedWeapons/ShrapnelCannon/ShrapnelCannon.gd
@@ -2,12 +2,10 @@ extends Weapon
 
 # Export variables allow you to set these in the editor
 @export var bullet_speed : float = 100.0
-@export var fire_rate : float = 0.25
+@export var fire_rate : float = 1
 
+@onready var bullet_spawn_marker: Marker3D = $BulletSpawn
 var can_shoot = true
-
-func _ready():
-	pass
 
 func _process(delta):
 	if can_shoot and Input.is_action_pressed("ranged_attack"):
@@ -18,14 +16,13 @@ func shoot():
 	#add child to character out of rotational root detached from any player transform
 	get_parent().get_parent().get_parent().add_child(bullet)
 	#get_tree().root.add_child(bullet)
-	print(bullet)
 
 	# Set the bullet's position to the gun's position
-	bullet.global_transform.origin = global_transform.origin
-
+	bullet.global_transform.origin = bullet_spawn_marker.global_transform.origin
+	bullet.shoot()
 	# Apply velocity to the bullet
-	var direction = global_transform.basis.z.normalized()
-	bullet.linear_velocity = direction * bullet_speed
+	#var direction = global_transform.basis.z.normalized()
+	#bullet.linear_velocity = direction * bullet_speed
 
 	# Implement fire rate
 	can_shoot = false
--- GodotProject/Scripts/Weapons/RangedWeapons/ShrapnelCannon/ShrapnelCannonBullet.gd
+++ GodotProject/Scripts/Weapons/RangedWeapons/ShrapnelCannon/ShrapnelCannonBullet.gd
@@ -3,12 +3,17 @@ extends Node3D
 
 var linear_velocity = Vector3.ZERO
 
-func _ready():
-	# Set the bullet to deactivate after a few seconds to prevent it from traveling indefinitely
-	#set_process(false)
-	await get_tree().create_timer(5.0).timeout
-	queue_free()
-
 func _process(delta):
 	# Move the bullet
 	translate(linear_velocity * delta)
+
+func shoot():
+	$GPUParticles3D.emitting = true
+
+
+func _on_gpu_particles_3d_finished():
+	queue_free()
+
+
+func _on_area_3d_body_entered(body):
+	print(body)
