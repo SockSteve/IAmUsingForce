0c759787ef9baeaa6a0afd2ea1be077897bc321f
Steffen Schmidt <50168768+SockSteve@users.noreply.github.com>
2024-08-07 06:48:59 +0200

    YOU CAN CHANGE THE LOOP VARIABLE ON PATHFOLLOW 3D FROM TRUE TO FALSE! WHY IS THE DEFAULT TRUE?

--- GodotProject/Scripts/Characters/Player/States/Grind.gd
+++ GodotProject/Scripts/Characters/Player/States/Grind.gd
@@ -7,71 +7,77 @@ var is_jumping_between_rails: bool = false
 var grind_jump: bool = false
 var grind_rail_change: bool = false
 var grind_jump_time: float = 0
+var path_end: bool = false
 
 #when we enter this state, we set the corresponding animation, get any grindrail related
 #information and set the initial progress to set the player on the correct point where he starts grinding
 func enter(_msg := {}) -> void:
 	player._character_skin.grind()
 	path_follow_3d = PathFollow3D.new()
+	path_follow_3d.loop = false
 	path_3d = player.get_inventory().get_gadget("GrindBoots").current_grindrail
 	path_3d.add_child(path_follow_3d)
 	set_initial_progress(player.global_transform.origin)
 
 #here we move the player along the path3d throug a path_follow3d
+#func update(_delta: float)->void:
+	#pass
 
 func physics_update(delta: float) -> void:
-	if path_3d != null:
-		#update player progress
-		path_follow_3d.progress_ratio += (delta * player.get_inventory().get_gadget("GrindBoots").grind_speed_time_factor)/path_3d.curve.get_baked_length()
+	if path_3d == null:
+		return
+	
+	#update player progress
+	path_follow_3d.progress_ratio += (delta * player.get_inventory().get_gadget("GrindBoots").grind_speed_time_factor)/path_3d.curve.get_baked_length()
+	
+	#move player through path_follow_3d
+	#we check for grindjump, because when jumping we update our y position, which would otherwise be resetted
+	if !grind_jump:
+		player.global_position = path_follow_3d.global_position
 		
-		#move player through path_follow_3d
-		#we check for grindjump, because when jumping we update our y position, which would otherwise be resetted
+	player._rotation_root.global_basis = path_follow_3d.global_basis.rotated(Vector3(0,1,0),PI)
+	
+	print(path_follow_3d.progress_ratio)
+	if path_follow_3d.progress_ratio >= 1:
+		if player.get_inventory().get_gadget("GrindBoots").is_looping:
+			path_follow_3d.progress_ratio = 0.0
+		else:
+			endGrind()
+		return
+	
+	
+	
+	#when we have a grindrails next to ours and detect them, we can jump to them 
+	if Input.is_action_just_pressed("jump"):
+		if Input.is_action_pressed("move_left") and player.get_inventory().get_gadget("GrindBoots").get_side_rail_path3d("left") != null:
+			change_grindrail("left")
+			grind_rail_change = true
+		elif Input.is_action_pressed("move_right") and player.get_inventory().get_gadget("GrindBoots").get_side_rail_path3d("right") != null:
+			change_grindrail("right")
+			grind_rail_change = true
+		#return
+	
+	if grind_rail_change:
+		grind_jump_time += delta
+		player.global_position = player.global_position.lerp(path_follow_3d.global_position, grind_jump_time)
+		if grind_jump_time >=1:
+			grind_jump_time = 0
+			grind_rail_change = false
+		return
+	
+	if Input.is_action_just_pressed("jump"):
 		if !grind_jump:
-			player.global_position = path_follow_3d.global_position
-			
-		player._rotation_root.global_basis = path_follow_3d.global_basis.rotated(Vector3(0,1,0),PI)
-		
-		print(path_follow_3d.progress_ratio)
-		if path_follow_3d.progress_ratio >= .95:
-			if player.get_inventory().get_gadget("GrindBoots").is_looping:
-				path_follow_3d.progress_ratio = 0.0
-			else:
-				endGrind()
-			return
-		
-		
-		
-		#when we have a grindrails next to ours and detect them, we can jump to them 
-		if Input.is_action_just_pressed("jump"):
-			if Input.is_action_pressed("move_left") and player.get_inventory().get_gadget("GrindBoots").get_side_rail_path3d("left") != null:
-				change_grindrail("left")
-				grind_rail_change = true
-			elif Input.is_action_pressed("move_right") and player.get_inventory().get_gadget("GrindBoots").get_side_rail_path3d("right") != null:
-				change_grindrail("right")
-				grind_rail_change = true
-			#return
-		
-		if grind_rail_change:
-			grind_jump_time += delta
-			player.global_position = player.global_position.lerp(path_follow_3d.global_position, grind_jump_time)
-			if grind_jump_time >=1:
-				grind_jump_time = 0
-				grind_rail_change = false
-			return
-		
-		if Input.is_action_just_pressed("jump"):
-			if !grind_jump:
-				grind_jump = true
-		
-		
-		if grind_jump:
-			player.global_position = path_follow_3d.global_position
-			grind_jump_time += delta * player.get_inventory().get_gadget("GrindBoots").grind_curve_time_factor
-			var jump_y_pos: float =  player.get_inventory().get_gadget("GrindBoots").grind_jump_curve.sample(grind_jump_time)
-			player.global_position.y = path_follow_3d.global_position.y + jump_y_pos
-			if grind_jump_time >= 1:
-				grind_jump_time = 0.0
-				grind_jump = false
+			grind_jump = true
+	
+	
+	if grind_jump:
+		player.global_position = path_follow_3d.global_position
+		grind_jump_time += delta * player.get_inventory().get_gadget("GrindBoots").grind_curve_time_factor
+		var jump_y_pos: float =  player.get_inventory().get_gadget("GrindBoots").grind_jump_curve.sample(grind_jump_time)
+		player.global_position.y = path_follow_3d.global_position.y + jump_y_pos
+		if grind_jump_time >= 1:
+			grind_jump_time = 0.0
+			grind_jump = false
 
 #when we first enter the grindrail, we adjust the progress_ratio of the path_follow3d to start grinding
 #on the impact point where we jumped on
@@ -102,6 +108,7 @@ func change_grindrail(dir):
 	#player.get_inventory().get_gadget("GrindBoots").clear_grindrail_exceptions()
 	player.can_grind=true
 	path_follow_3d = PathFollow3D.new()
+	path_follow_3d.loop = false
 	path_3d.add_child(path_follow_3d)
 	set_initial_progress(player.global_position)
 	#player.get_inventory().get_gadget("GrindBoots").end_grind()
