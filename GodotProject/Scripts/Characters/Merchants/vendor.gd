d2cc4e5b700430f2916cdffb9581e18e738c1e64
Steffen Schmidt <50168768+SockSteve@users.noreply.github.com>
2024-09-04 21:29:32 +0200

    moreUi stuff

--- GodotProject/Scripts/Characters/Merchants/vendor.gd
+++ GodotProject/Scripts/Characters/Merchants/vendor.gd
@@ -5,8 +5,10 @@ signal costumer_left()
 
 var customer : Player = null
 var player_cam : Camera3D = null
-var shop_menu : bool = false
+var player_is_shopping: bool = false
+@onready var shop_menu : Node3D = $ShopMenu3D
 @onready var vendor_cam : Camera3D = $VendorCamera3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,15 +18,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("interact"):
-		shop_menu = not shop_menu
+		player_is_shopping = not player_is_shopping
 		
-		if shop_menu:
+		if player_is_shopping:
 			open_shop()
 			
-		if not shop_menu:
+		if not player_is_shopping:
 			close_shop()
 
 func open_shop():
+	shop_menu.visible = true
+	animation_player.play("open_shop")
+	get_tree().paused = true
 	customer.freeze = true
 	emit_signal("got_costumer", customer)
 	vendor_cam.make_current()
@@ -32,12 +37,16 @@ func open_shop():
 	$ShopMenu3D.get_shop_handler().initialize()
 
 func close_shop():
+	get_tree().paused = false
 	emit_signal("costumer_left")
 	$ShopMenu3D.get_shop_handler().cleanup()
 	customer.freeze = false
 	player_cam.make_current()
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	customer.set_up_input()
+	animation_player.play_backwards("open_shop")
+	await animation_player.animation_finished
+	shop_menu.visible = false
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("player"):
